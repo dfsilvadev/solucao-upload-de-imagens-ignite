dd042ea1240133069ba611a38d77b47a35987482
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-21 11:37:27 -0300

    feat: useInfiniteQuery, getNextPageParam e as regras da page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,29 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+type ImagesResponse = {
+  data: Image[];
+  after: string;
+};
+
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<ImagesResponse> {
+    const response = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return response.data;
+  }
   const {
     data,
     isLoading,
@@ -16,20 +38,23 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.after ?? null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +62,16 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            type="button"
+            onClick={() => fetchNextPage()}
+            loadingText="Carregando..."
+            isLoading={isFetchingNextPage}
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
